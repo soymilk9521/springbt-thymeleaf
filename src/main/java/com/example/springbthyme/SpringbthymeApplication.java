010f0361bd9fc3e29e4a5b04d67f8dd04d8eb619
r_li <r_li@globals.jp>
2017-12-14 17:48:40 +0900

    spring boot demo

---
+++ src/main/java/com/example/springbthyme/SpringbthymeApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbthyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbthymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbthymeApplication.class, args);
+	}
+}
