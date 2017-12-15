9191411b5650cc2ab8ace5398668f13ed24b7ab4
r_li <r_li@globals.jp>
2017-12-15 16:38:00 +0900

    spring boot paramater

--- src/main/java/com/example/springbthyme/controller/HomeController.java
+++ src/main/java/com/example/springbthyme/controller/HomeController.java
@@ -1,20 +1,33 @@
 package com.example.springbthyme.controller;
 
+import org.springframework.util.StringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
-
     @RequestMapping("/")
-    public String index() {
-        System.out.println("index() is running!");
+    public String index(ModelMap map) {
+        List<String> strList = new ArrayList<String>();
+        strList.add("Java");
+        strList.add("Sping Boot");
+        strList.add("Scala");
+        map.addAttribute("name", strList);
         return "index";
     }
 
-    @RequestMapping("/home")
-    public String home() {
-        System.out.println("home() is running!");
+    @RequestMapping(value={"/home", "/home/{name}"}, method = RequestMethod.GET)
+    public String home(Model model, @PathVariable(value="name", required = false) String name) {
+        if (!StringUtils.isEmpty(name)){
+            model.addAttribute("name", name);
+        }
         return "home";
     }
 }
